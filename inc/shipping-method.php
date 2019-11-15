2f0669cf85fa289b51fced55b835a25ad63cdf22
forhad-h <hosainf1@gmail.com>
2019-11-15 23:49:01 +0600

    Prevent to show my shipping title when it's empty in cart page

--- inc/shipping-method.php
+++ inc/shipping-method.php
@@ -23,12 +23,18 @@ function wspp_shipping_method_init() {
        * @return void
       */
       public function __construct() {
+        /**
+         * get the shipping value if exist otherwise make it 0
+         * @since 1.0.0
+         * @var number shipping cost
+        */
+        $shipping_cost = count(get_shipping_cost_arr()) > 0 ? max(get_shipping_cost_arr()) : 0;
 
         $this->id = 'wc_standard_shipping_method';
         $this->method_title = 'Standard Shipping';
         $this->method_description = 'Standard flat rate shipping per product';
         $this->title = "Standard Shipping";
-        $this->enabled = 'yes';
+        $this->enabled = $shipping_cost ? 'yes' : 'no'; // enabled if $shipping_cost exists
         $this->init();
 
       }
@@ -58,20 +64,10 @@ function wspp_shipping_method_init() {
        * @return void
       */
       public function calculate_shipping( $package = array() ) {
-        global $woocommerce;
-        $items = $woocommerce->cart->get_cart();
-        $cost_arr = [];
-
-        foreach($items as $item => $values) {
-          $product = wc_get_product( $values[ 'data' ]->get_id() );
-          $cost = ( float ) $product->get_meta( 'wspp_standard_shipping_cost' );
-          array_push($cost_arr, $cost);
-        }
-
         $rate = [
           'id' => $this->id,
           'label' => $this->title,
-          'cost' => max($cost_arr),
+          'cost' => max(get_shipping_cost_arr()),
           'calc_tax' => 'per_order',
         ];
 
@@ -87,7 +83,24 @@ add_action( 'woocommerce_shipping_init', 'wspp_shipping_method_init');
 
 
 function add_wc_standard_shipping_method($methods) {
+
   $methods['wc_standard_shipping_method'] = 'WC_Standard_Shipping_Method';
+
   return $methods;
 }
 add_filter('woocommerce_shipping_methods', 'add_wc_standard_shipping_method');
+
+function get_shipping_cost_arr() {
+  global $woocommerce;
+
+  if(!$woocommerce->cart) return [];
+  $items = $woocommerce->cart->get_cart();
+  $cost_arr = [];
+
+  foreach($items as $item => $values) {
+    $product = wc_get_product( $values[ 'data' ]->get_id() );
+    $cost = ( float ) $product->get_meta( 'wspp_standard_shipping_cost' );
+    array_push($cost_arr, $cost);
+  }
+  return $cost_arr;
+}
